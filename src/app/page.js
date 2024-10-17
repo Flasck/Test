6206f300183a4d729928cc71f07592e676ea97e7
Nikulin Vladislav / Никулин Владислав <po_6706@mail.ru>
2024-10-17 18:13:01 +0400

    gh6

--- src/app/page.js
+++ src/app/page.js
@@ -2,5 +2,5 @@ import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
-	return <div>1234567890</div>
+	return <div>Рамиль пидорас в меня не верил а я все равно заебениу красоту</div>
 }
