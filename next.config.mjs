6a224500425efc31e6f55773573661e584cd7901
Nikulin Vladislav / Никулин Владислав <po_6706@mail.ru>
2024-10-17 17:43:24 +0400

    config2

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
-const isProd = process.env.NODE_ENV === 'production'
+/** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
-	basePath: isProd ? '/Test' : '',
-	assetPrefix: isProd ? '/Test/' : '',
+	basePath: isProd ? '/имя_вашего_репозитория' : '',
+	assetPrefix: isProd ? '/имя_вашего_репозитория/' : '',
 	trailingSlash: true,
 }
 
-module.exports = nextConfig
+export default nextConfig
